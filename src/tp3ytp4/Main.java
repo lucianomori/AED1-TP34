64c8563d20b63870704bd3ee32dab50c9d4bf689
Luciano Mori <smluciano@gmail.com>
2018-04-29 22:27:16 -0300

    Initial commit - TP3 euclides

---
+++ src/tp3ytp4/Main.java
@@ -0,0 +1,39 @@
+package tp3ytp4;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long numero1 ;
+		long numero2 ;
+		long resultado;
+		
+		Scanner leerlong = new Scanner(System.in);	
+		//System.out.println("3%6 es: "+(3%6)+" 3/6 es: "+(3/6));
+		
+		System.out.println("\n** TP3 ** \nIngrese dos numeros y averiguaremos si son coprimos\n\nIngrese el primer numero");
+		numero1  = leerlong.nextLong();
+		
+		System.out.println("\nIngrese el segundo numero");
+		numero2  = leerlong.nextLong();
+		
+		resultado = mcd(numero1,numero2);
+		
+		if (resultado == 1){
+			System.out.println("\nLos numeros son coprimos! ");
+		}else {
+			System.out.println("\nLos numeros NO son coprimos.");
+			System.out.println("Su máximo comun divisor es " + resultado);
+		}
+}
+
+	
+public static long  mcd(long a,long b) 
+	{
+		if (b == 0) 
+			return a; 
+		else 
+			return mcd(b, a%b);
+		}
+}
